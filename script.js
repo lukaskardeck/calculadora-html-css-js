98e376cfa8735905e28f6ac73fc92bb8f2831e04
lukaskardeck <92531036+lukaskardeck@users.noreply.github.com>
2024-08-01 09:21:25 -0300

    :sparkles: feat: deletando ultimo numero inserido

--- script.js
+++ script.js
@@ -1,42 +1,52 @@
 // Elementos da tela
 const elementos = {
-    displayResultado: document.querySelector('.resultado'),
-    displayIgual: document.querySelector('.opIgual'),
-    displayMenor: document.querySelector('.displayMenor'),
-    numeros: document.querySelectorAll('.num'),
-    operadores: document.querySelectorAll('.operador'),
-    deletar: document.querySelector('.deletar'),
-    ponto: document.querySelector('.ponto'),
-    resetar: document.querySelector('.resetar'),
-    igual: document.querySelector('.igual')
-}
+  displayResultado: document.querySelector(".resultado"),
+  displayIgual: document.querySelector(".opIgual"),
+  displayMenor: document.querySelector(".displayMenor"),
+  numeros: document.querySelectorAll(".num"),
+  operadores: document.querySelectorAll(".operador"),
+  deletar: document.querySelector(".deletar"),
+  ponto: document.querySelector(".ponto"),
+  resetar: document.querySelector(".resetar"),
+  igual: document.querySelector(".igual"),
+};
 
 // variáveis globais
 let valorAtual = "0";
 
 // Funções
 function atualizaDisplayResultado() {
-    elementos.displayResultado.textContent = valorAtual;
+  elementos.displayResultado.textContent = valorAtual;
 }
 
 function insereNumero(evento) {
-    if (valorAtual === "0" ) {
-        valorAtual = evento.target.textContent;
-    } else {
-        valorAtual += evento.target.textContent;
-    }
+  if (valorAtual === "0") {
+    valorAtual = evento.target.textContent;
+  } else {
+    valorAtual += evento.target.textContent;
+  }
 
-    atualizaDisplayResultado();
+  atualizaDisplayResultado();
 }
 
 function limparDisplay() {
-    valorAtual = "0";
+  valorAtual = "0";
+  atualizaDisplayResultado();
+}
+
+function deletarUltimo() {
+  if (valorAtual !== "0") {
+    if (valorAtual.length === 0) valorAtual = "0";
+    else valorAtual = valorAtual.substring(0, valorAtual.length() - 1);
     atualizaDisplayResultado();
+  }
 }
 
 // Eventos
 elementos.numeros.forEach((num) => {
-    num.addEventListener('click', insereNumero);
+  num.addEventListener("click", insereNumero);
 });
 
-elementos.resetar.addEventListener('click', limparDisplay)
+elementos.resetar.addEventListener("click", limparDisplay);
+
+elementos.deletar.addEventListener('click', deletarUltimo);

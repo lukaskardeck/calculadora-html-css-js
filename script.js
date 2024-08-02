ffd63a6a8ec7c7b201d3951cbaed63b23e17eaad
lukaskardeck <92531036+lukaskardeck@users.noreply.github.com>
2024-08-02 10:05:05 -0300

    :sparkles: feat: adicionando operadores e realizando as operações

--- script.js
+++ script.js
@@ -11,34 +11,130 @@ const elementos = {
   igual: document.querySelector(".igual"),
 };
 
-// variáveis globais
-let valorAtual = "0";
+// Variáveis globais
+let valorAtual = "";
+let valorAnterior = "";
+let calculando = false;
+let operador = null;
 
 // Funções
 function atualizaDisplayResultado() {
   elementos.displayResultado.textContent = valorAtual;
 }
 
-function insereNumero(evento) {
-  if (valorAtual === "0") {
-    valorAtual = evento.target.textContent;
+function atualizaDisplayMenor() {
+  if (operador !== null) {
+    elementos.displayMenor.textContent = valorAnterior + operador;
   } else {
-    valorAtual += evento.target.textContent;
+    elementos.displayMenor.textContent = "";
+  }
+}
+
+function atualizaDisplayIgual(isIgual) {
+  if (isIgual) elementos.displayIgual.textContent = "=";
+  else elementos.displayIgual.textContent = "";
+}
+
+function insereNumero(evento) {
+  if (valorAtual.length < 8) {
+    if (calculando) {
+      valorAtual = evento.target.textContent;
+      calculando = false;
+    } else {
+      if (valorAtual === "0" ) {
+        valorAtual = evento.target.textContent;
+      } else {
+        valorAtual += evento.target.textContent;
+      }
+    }
+
+    atualizaDisplayIgual(false);
+    atualizaDisplayResultado();
+  }
+}
+
+function insereOperador(evento) {
+  if (!valorAtual.endsWith(".")) {
+    if (operador !== null && !calculando && valorAtual !== "") {
+      calcular();
+    }
+    if (valorAtual !== "") {
+      valorAnterior = valorAtual;
+      valorAtual = "";
+    }
+    operador = evento.target.textContent;
+    atualizaDisplayMenor();
+    atualizaDisplayResultado();
+    atualizaDisplayIgual(false);
+  }
+}
+
+function calcular() {
+  let resultado;
+  const anterior = Number(valorAnterior);
+  const atual = Number(valorAtual);
+
+  switch (operador) {
+    case "+":
+      resultado = anterior + atual;
+      break;
+    case "-":
+      resultado = anterior - atual;
+      break;
+    case "*":
+      resultado = anterior * atual;
+      break;
+    case "/":
+      if (atual !== 0) {
+        resultado = anterior / atual;
+      } else {
+        alert("Divisão por zero não é permitida");
+        return;
+      }
+      break;
+    default:
+      return;
   }
 
+  valorAtual = resultado.toString();
+  valorAnterior = "";
+  operador = null;
+  calculando = true;
   atualizaDisplayResultado();
+  atualizaDisplayMenor();
 }
 
 function limparDisplay() {
-  valorAtual = "0";
-  atualizaDisplayResultado();
+  valorAtual = "";
+  valorAnterior = "";
+  operador = null;
+  calculando = false;
+  elementos.displayIgual.textContent = "";
+  elementos.displayMenor.textContent = "";
+  elementos.displayResultado.textContent = "";
 }
 
 function deletarUltimo() {
   if (valorAtual !== "0") {
-    if (valorAtual.length === 0) valorAtual = "0";
-    else valorAtual = valorAtual.substring(0, valorAtual.length() - 1);
+    if (valorAtual.length === 1) valorAtual = "0";
+    else valorAtual = valorAtual.slice(0, -1);
     atualizaDisplayResultado();
+  }
+}
+
+function adicionaPonto() {
+  if (!valorAtual.includes(".")) {
+    valorAtual += ".";
+    atualizaDisplayResultado();
+  }
+}
+
+function calculaIgual() {
+  if (operador !== null) {
+    calcular();
+    operador = null;
+    elementos.displayMenor.textContent = "";
+    atualizaDisplayIgual(true);
   }
 }
 
@@ -47,6 +143,14 @@ elementos.numeros.forEach((num) => {
   num.addEventListener("click", insereNumero);
 });
 
+elementos.operadores.forEach((op) => {
+  op.addEventListener("click", insereOperador);
+});
+
 elementos.resetar.addEventListener("click", limparDisplay);
 
-elementos.deletar.addEventListener('click', deletarUltimo);
+elementos.deletar.addEventListener("click", deletarUltimo);
+
+elementos.ponto.addEventListener("click", adicionaPonto);
+
+elementos.igual.addEventListener("click", calculaIgual);
